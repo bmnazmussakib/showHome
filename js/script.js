f9f292b32fc333bc293771e45f593ec3ffe2a3a3
Nazmus Sakib <nazmussakib161@gmail.com>
2022-07-26 20:52:41 +0600

    first

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      nav: true,
+      items: 3,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      loop: true,
+      navText: ["<i class='fa-solid fa-chevron-left'></i>","<i class='fa-solid fa-chevron-right'></i>"],
+    });
+  });
+   
+    
+  });
